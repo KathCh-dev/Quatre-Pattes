86a2cdc835f08730b4f08fd10027f109e01b76a3
Kathleen <kathleen.chaiffre.pro@gmail.com>
2025-02-19 14:20:45 +0100

    intégration de la section 5 (quizz et impact) et de son code CSS

---
+++ accueil.js
@@ -0,0 +1,11 @@
+// Questions du Quizz:
+
+// // Les associations gardent la majorité des dons pour leur fonctionnement
+// Mauvaise réponse : "Vrai, les associations utilisent plus de 50% des dons pour leurs frais de fonctionnement"
+// Bonne réponse : "Faux ! Chez Quatre Pattes, 85% des dons sont directement utilisés pour les animaux. Seuls 15% servent aux frais de fonctionnement essentiels."
+// Les refuges ne prennent que les animaux en bonne santé
+// Mauvaise réponse : "Vrai, ils refusent les animaux malades car c'est trop coûteux"
+// Bonne réponse : "Faux ! Nous accueillons tous les animaux, quel que soit leur état de santé. Chaque
+// Les associations ont trop de moyens grâce aux dons
+// Mauvaise réponse : "Vrai, elles reçoivent beaucoup d'argent des donateurs"
+// Bonne réponse: "Faux ! Les besoins sont immenses et constants. Chaque don est précieux pour sauver plus d'animaux."
